3b1d5ac660da87b0bd8291a1babfcfe197a20188
JUNHOULIN <yy6313531@gmail.com>
2024-11-08 16:16:46 +0800

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <script setup></script>
 
 <template>
-  <h1>Hello world</h1>
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
+  <button type="button" class="btn btn-link">Link</button>
 </template>
 
 <style></style>
